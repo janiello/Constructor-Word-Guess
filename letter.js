0aedd1b7ef1487e545d7bffe84d315cb3430c41c
janiello <janiello417@gmail.com>
2019-05-22 16:26:10 -0400

    wrote code for letter.js. Took process.argv as the argument to test

---
+++ letter.js
@@ -0,0 +1,19 @@
+var letter = {
+    character: "a",
+    isGuessed: false,
+    showLetter: function() {
+        if (this.isGuessed === true) {
+            console.log(this.character);
+        } else {
+            console.log("_");
+        };
+    },
+    userGuess: function(guess) {
+        if (guess === this.character) {
+            this.isGuessed = true
+        };
+    }
+};
+
+letter.userGuess(process.argv[2]);
+letter.showLetter();
